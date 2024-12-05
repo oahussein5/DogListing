62478d2758ca45b2037c3c693556c52d9f76cde9
omar <omar.hussein@ndsu.edu>
2024-12-04 20:26:48 -0600

    Created Dog Class

--- Components/Models/Dog.cs
+++ Components/Models/Dog.cs
@@ -2,6 +2,11 @@
 {
     public class Dog
     {
-//DOG CLASS
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string ImageUrl { get; set; } // URL for dog image
+        public bool IsAdopted { get; set; } // Whether the dog is already adopted
     }
 }
